38fd57226c957183dcf5749aa8f9f593abf40731
IzuMan0x <mcasterchief@gmail.com>
2023-09-22 08:31:43 +0900

    testing deployment

--- next.config.js
+++ next.config.js
@@ -2,12 +2,12 @@
 const nextConfig = {
   reactStrictMode: true,
   distDir: "dist",
-  //output: "export",
-  exportPathMap: function () {
-    return {
-      "/": { page: "/" },
-    };
-  },
 };
 
 module.exports = nextConfig;
+
+/* exportPathMap: function () {
+    return {
+      "/": { page: "/" },
+    };
+  }, */
